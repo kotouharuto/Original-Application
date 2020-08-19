5bc90051b1153531db36713d38b61c7fe4dd72a4
ことうはると <haruto.k.0321@gmail.com>
2020-08-19 16:32:39 +0900

    挿入処理をfunction.phpに移動

--- scr-image/libs/fucntion.php
+++ scr-image/libs/fucntion.php
@@ -32,10 +32,10 @@ function fetchAllMenus($pdo)
 }
 
 //メニュー追加
-function addTrainingMenu($menu, $set, $set_num)
-{
+// function addTrainingMenu($menu, $set, $set_num)
+// {
 
-}
+// }
 
 //メニュー削除
 function deleteTrainingMenu($pdo, $id)
@@ -77,9 +77,30 @@ function EmptNumCheck(){
         print '<br>';
         print '<br>';
         if(isset($_POST['menu'])) {
-            header("Location:menupost.php");
+            // header("Location:menupost.php");
         }
     }
+}
+
+//挿入処理
+// 
+function INSERT() {
+    try {
+        $pdo = db_connect();
+        $pdo->beginTransaction();
+        $sql = "INSERT INTO trainingmenu (menu, num, setnum) VALUES (:menu, :num, :setnum)";
+        $stmh = $pdo->prepare($sql);
+        $stmh->bindValue(':menu', $_POST['menu'], PDO::PARAM_STR);
+        $stmh->bindValue(':num', $_POST['num'], PDO::PARAM_STR);
+        $stmh->bindValue(':setnum', $_POST['setnum'], PDO::PARAM_STR);
+        $stmh->execute();
+        $pdo->commit();
+        // header('Location:menupost.php');
+    } catch (PDOException $Exception) {
+        $pdo->rollback();
+        print  "error：". $Exception->getMessage();
+    }
+    return $pdo;
 }
 
 //Smarty接続
