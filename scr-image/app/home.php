2ea626daef76cb82ab1304305521735c6736cb40
ことうはると <haruto.k.0321@gmail.com>
2020-08-30 14:34:26 +0900

    home.php (ログイン後にリダイレクトされる会員ページ) の追加

--- scr-image/app/home.php
+++ scr-image/app/home.php
@@ -1,10 +1,13 @@
 <?php
-
 require_once '../libs/init.php';
+// $pdo = db_connect();
+// $sql = "SELECT * FROM users";
+// $stmt = $pdo->prepare($sql);
+// $row = $stmt->fetch(PDO::FETCH_ASSOC);
+// $stmt->execute();
 
-$pdo = db_connect();
-
+echo "こんにちは". htmlspecialchars($_SESSION['USERNAME']) ."さん。今日も筋トレを楽しみましょう。";
 
 
 $smarty = getSmarty();
-$smarty->display('../libs/templates/login.tpl');
+$smarty->display('../libs/templates/home.tpl');
