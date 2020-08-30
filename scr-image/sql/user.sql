ec39d237c3de59ae7b6640f848156700df1aa63b
ことうはると <haruto.k.0321@gmail.com>
2020-08-30 14:27:56 +0900

    細かな編集

--- scr-image/sql/user.sql
+++ scr-image/sql/user.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+--- phpMyAdmin SQL Dump
 -- version 4.9.3
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: localhost:8889
--- 生成日時: 2020 年 7 月 29 日 10:26
+-- 生成日時: 2020 年 8 月 26 日 08:10
 -- サーバのバージョン： 5.7.26
 -- PHP のバージョン: 7.4.2
 
@@ -21,10 +21,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `password` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -35,7 +35,7 @@ CREATE TABLE `users` (
 -- テーブルのインデックス `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- ダンプしたテーブルのAUTO_INCREMENT
@@ -45,5 +45,4 @@ ALTER TABLE `users`
 -- テーブルのAUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-  
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
