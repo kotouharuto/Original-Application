66fe45e00a1cf278d7b1633d0ceeff0b69e6adc8
ことうはると <haruto.k.0321@gmail.com>
2020-08-30 14:33:24 +0900

    カレンダー機能 UI作成

--- scr-image/app/calendar.php
+++ scr-image/app/calendar.php
@@ -1,152 +1,147 @@
 <?php
-// カレンダーを表示する年を指定する
-$year = 2020;
+require_once '../libs/init.php';
+// タイムゾーンを設定
+date_default_timezone_set('Asia/Tokyo');
 
-// 年始のタイムスタンプを取得
-$timestamp = strtotime("{$year}-01-01 00:00:00");
-$weekday = 0;
+// 前月・次月リンクが押された場合は、GETパラメーターから年月を取得
+if (isset($_GET['ym'])) {
+    $ym = $_GET['ym'];
+} else {
+    // 今月の年月を表示
+    $ym = date('Y-m');
+}
+
+// タイムスタンプを作成し、フォーマットをチェックする
+$timestamp = strtotime($ym . '-01');
+if ($timestamp === false) {
+    $ym = date('Y-m');
+    $timestamp = strtotime($ym . '-01');
+}
+
+// 今日の日付 フォーマット　例）2018-07-3
+$today = date('Y-m-j');
+
+// カレンダーのタイトルを作成　例）2017年7月
+$html_title = date('Y年n月', $timestamp);
+
+// 前月・次月の年月を取得
+// 方法１：mktimeを使う mktime(hour,minute,second,month,day,year)
+$prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
+$next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
+
+// 方法２：strtotimeを使う
+// $prev = date('Y-m', strtotime('-1 month', $timestamp));
+// $next = date('Y-m', strtotime('+1 month', $timestamp));
+
+// 該当月の日数を取得
+$day_count = date('t', $timestamp);
+
+// １日が何曜日か　0:日 1:月 2:火 ... 6:土
+// 方法１：mktimeを使う
+$youbi = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
+// 方法２
+// $youbi = date('w', $timestamp);
+
+
+// カレンダー作成の準備
+$weeks = [];
+$week = '';
+
+// 第１週目：空のセルを追加
+// 例）１日が水曜日だった場合、日曜日から火曜日の３つ分の空セルを追加する
+$week .= str_repeat('<td></td>', $youbi);
+
+for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
+
+    // 2017-07-3
+    $date = $ym . '-' . $day;
+
+    if ($today == $date) {
+        // 今日の日付の場合は、class="today"をつける
+        $week .= '<td class="today">' . $day;
+    } else {
+        $week .= '<td>' . $day;
+    }
+    $week .= '</td>';
+
+    // 週終わり、または、月終わりの場合
+    if ($youbi % 7 == 6 || $day == $day_count) {
+
+        if ($day == $day_count) {
+            // 月の最終日の場合、空セルを追加
+            // 例）最終日が木曜日の場合、金・土曜日の空セルを追加
+            $week .= str_repeat('<td></td>', 6 - ($youbi % 7));
+        }
+
+        // weeks配列にtrと$weekを追加する
+        $weeks[] = '<tr>' . $week . '</tr>';
+
+        // weekをリセット
+        $week = '';
+	}
+}
+
+// $smarty = getSmarty();
+// $smarty->assign('weeks', $weeks);
+// $smarty->assign('ym', $ym);
+// $smarty->assign('prev', $prev);
+// $smarty->assign('next', $next);
+// $smarty->display('../libs/templates/calendar.tpl');
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="utf-8">
-<title>カレンダー</title>
-<style>
-main {
-	display: flex;
-	flex-wrap: wrap;
-}
-
-.yearcount {
-    text-align: left;
-    font-size: 15px; 
-}
-
-.titleday {
-    font-size: 25px;
-    text-align: center;
-}
-
-table {
-	text-align: center;
-	margin-bottom: 20px;
-	border: 1px solid #555;
-	margin: 0 auto;
-	margin-bottom: 15px;
-}
-
-table caption {
-	padding: 7px 0;
-	font-size: 86%;
-	color: black;
-	line-height: 1.0em;
-	background: white;
-	border: 1px solid black;
-}
-table th,
-table td {
-	margin-right: 5px;
-	padding: 0 5px;
-	font-size: 86%;
-	color: #222;
-	text-align: center;
-	letter-spacing: 5px;
-}
-table td {
-	text-align: right;
-}
-
-h1 {
-	text-align: center;
-}
-
-</style>
+    <meta charset="utf-8">
+    <title>PHPカレンダー</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+    <style>
+        .container {
+            font-family: 'Noto Sans JP', sans-serif;
+            margin-top: 80px;
+        }
+        h3 {
+            margin-bottom: 30px;
+        }
+        th {
+            height: 30px;
+            text-align: center;
+        }
+        td {
+            height: 100px;
+        }
+        .today {
+            background: orange;
+        }
+        th:nth-of-type(1), td:nth-of-type(1) {
+            color: red;
+        }
+        th:nth-of-type(7), td:nth-of-type(7) {
+            color: blue;
+        }
+    </style>
 </head>
 <body>
-    <p class="titleday">日にちを選択してください</p>
-	<h2 class="yearcount"><?php echo $year .'年'; ?></h2>
-<main>
-	<?php for( $i=1; $i<=12; $i++): ?>
-	<table class="calender">
-		<caption><?php echo $i; ?>月</caption>
-		<thead>
-			<tr>
-				<th>日</th><th>月</th><th>火</th><th>水</th>
-				<th>木</th><th>金</th><th>土</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			// 開始日を取得
-			$start_day_timestamp = mktime(0,0,0,$i,1,$year);
-		
-			// 末日を取得
-			$end_day_timestamp = mktime(0,0,0,($i+1),0,$year);
-			$end_day = date("d", $end_day_timestamp);
-	
-			//現在の月を取得
-			$nowmonth = date("n");
-
-			//現在の日にちを取得
-			$nowday = date("j");
-
-			//1日から月末までを表示する処理
-			for( $j=1; $j <= $end_day; $j++ ) {
-				if( $j === 1 ) {
-		
-					echo '<tr>';
-		
-					$weekday = date("w", $start_day_timestamp);
-					
-					for( $k=0; $k<$weekday; $k++) {
-						echo '<td></td>';
-					} 
-				}
-
-				switch($weekday) {
-				case 0:
-					$style = 'color: red;';
-				break;
-				case 6:
-					$style = 'color: blue;';
-				break;
-				default;
-				$style = 'color: #222;';
-				}
-			
-				if($i == $nowday) {
-
-				}
-
-				if($i == $nowmonth) {
-				}
-				
-				if($j == $nowday and $i == $nowmonth) {
-					$style = $style. "; background:red ";
-					$style = $style. "; color:white ";
-					$style = $style. "; width:30px ";
-					$style = $style. "; height:40px ";
-					$style = $style. "; border-radius:50% ";
-				}
-
-				echo '<td style="'.$style.'">'.$j.'</td>';
-
-				$weekday++;
-		
-				if( 6 < $weekday ) {
-					echo '</tr><tr>';
-					$weekday = 0;
-				}
-
-				
-			}
-			$style = 'background: red; width: 20px; height: 30px; border-radius: 50%;';
-			$stylec = 'background: black;';
-			?>
-		</tbody>
-	</table>
-	<?php endfor; ?>
-</main>
+    <div class="container">
+        <h3><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</a></h3>
+        <table class="table table-bordered">
+            <tr>
+                <th>日</th>
+                <th>月</th>
+                <th>火</th>
+                <th>水</th>
+                <th>木</th>
+                <th>金</th>
+                <th>土</th>
+            </tr>
+            <?php
+                foreach ($weeks as $week) {
+                    ?>
+                    <a href="menupost.php"><?php echo $week ?></a>
+                    <?php
+                }
+                ?>
+        </table>
+    </div>
 </body>
 </html>
--- scr-image/libs/templates/calendar.tpl
+++ scr-image/libs/templates/calendar.tpl
@@ -1,1 +1,55 @@
 {* Smarty_html/calendar.tpl *}
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>PHPカレンダー</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+    <style>
+        .container {
+            font-family: 'Noto Sans JP', sans-serif;
+            margin-top: 80px;
+        }
+        h3 {
+            margin-bottom: 30px;
+        }
+        th {
+            height: 30px;
+            text-align: center;
+        }
+        td {
+            height: 100px;
+        }
+        .today {
+            background: orange;
+        }
+        th:nth-of-type(1), td:nth-of-type(1) {
+            color: red;
+        }
+        th:nth-of-type(7), td:nth-of-type(7) {
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h3><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</a></h3>
+        <table class="table table-bordered">
+            <tr>
+                <th>日</th>
+                <th>月</th>
+                <th>火</th>
+                <th>水</th>
+                <th>木</th>
+                <th>金</th>
+                <th>土</th>
+            </tr>
+                {foreach from=$weeks item=$week}
+                    <td>{$week}</td>
+                {/foreach}
+        </table>
+    </div>
+</body>
+</html>
