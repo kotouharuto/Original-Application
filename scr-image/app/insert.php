06608bb349176a36ad69dc3c0f69969ea96b88f4
ことうはると <haruto.k.0321@gmail.com>
2020-09-13 11:09:10 +0900

    メールアドレス重複チェック

--- scr-image/app/insert.php
+++ scr-image/app/insert.php
@@ -1,21 +1,23 @@
 <?php
 require_once '../libs/init.php';
-
-if(isset($_POST['insert'])) {
-    if(isset())
-    $pdo = db_connect();
-    $pdo->beginTransaction();
-    $user_id = $_SESSION['user_id'];
-    $date = $_POST['date'];
-    $menu = $_POST['menu'];
-    $num = $_POST['num'];
-    $setnum = $_POST['setnum'];
-    try {
-        INSERT($pdo, $date, $user_id, $menu, $num, $setnum);
-        header('Location: menupost.php?date='. $date);
-    } catch(PDOException $Exception) {
-        $pdo->rollback();
-        print 'エラー：' . $Exception->getMessage();
+$request = new Request();
+$date = Request::get('date');
+if(isset($_REQUEST['insert'])) {
+    $sql =  "SELECT email FROM users WHERE email = :email";
+    if($sql) {
+        $pdo = db_connect();
+        $pdo->beginTransaction();
+        $user_id = $_SESSION['user_id'];
+        // $date = $_POST['date'];
+        $menu = $_POST['menu'];
+        $num = $_POST['num'];
+        $setnum = $_POST['setnum'];
+        try {
+            INSERT($pdo, $date, $user_id, $menu, $num, $setnum);
+            header('Location: menupost.php?date='. $date);
+        } catch(PDOException $Exception) {
+            $pdo->rollback();
+            print 'エラー：' . $Exception->getMessage();
+        }
     }
 }
-?>
