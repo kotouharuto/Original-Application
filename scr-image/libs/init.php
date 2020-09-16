f1257a41164a2b34cf3e8ee690545946dd4b4c3e
ことうはると <haruto.k.0321@gmail.com>
2020-09-16 15:57:10 +0900

    0910 修正 https://qiita.com/zaburo/items/eacde522d36172d6d910

--- scr-image/libs/init.php
+++ scr-image/libs/init.php
@@ -1,9 +1,18 @@
 <?php
 const APPLICATION_DIR = __DIR__. '/../';
 require_once APPLICATION_DIR. 'libs/smarty/Smarty.class.php';
+require_once __DIR__.'/vendor/autoload.php';
 require_once APPLICATION_DIR. 'libs/function.php';
 require_once APPLICATION_DIR. 'libs/classes/AuthUser.php';
 require_once APPLICATION_DIR. 'libs/classes/SessionManager.php';
 require_once APPLICATION_DIR. 'libs/classes/Request.php';
 
 session_start();
+
+//require
+
+//use
+use Sample\sampleClass;
+
+$sc = new sampleClass;
+$sc->Hello();
