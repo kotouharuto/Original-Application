bf12e470de5abce5e210389eaabd54546a8df3e7
ことうはると <haruto.k.0321@gmail.com>
2020-09-21 11:42:17 +0900

    コメントの追加

--- scr-image/app/menupost.php
+++ scr-image/app/menupost.php
@@ -1,19 +1,28 @@
 <?php
 require_once '../libs/init.php';
-require_once '../libs/function.php';
 
-use App\Request;
-// $request = new Request();
+// ログインしていなかったらログインしてもらう
 if(!isset($_SESSION['user_id'])) {
     header("Location: login.php");
 }
 
-$datetime = $_REQUEST['date'];
+// エラーがあったら表示
+if (isset($_GET['error'])) {
+    echo $_GET['error'];
+}
 
+
+use App\Request;
+
+$datetime = Request::get('date');
+
+// $datetimeをただの文字じゃなくする
 $date = date('Ymd',strtotime($datetime));
+//menupost.phpで日付を表示するための変数
 $datetext = date('Y年m月d日',strtotime($datetime));
 
-if(isset($date)) {
+// $datetimeを受け取っている場合
+if(isset($datetime)) {
     try {
         $pdo = db_connect();
         $menus = fetchAllMenus($pdo, $_SESSION['user_id'], $date);
@@ -21,11 +30,14 @@ if(isset($date)) {
         print "error：". $Exception->getMessage();
     }
     
-    $smarty = getSmarty();
-    $smarty->assign('menus', $menus);
-    $smarty->assign('date', $date);
-    $smarty->assign('datetext', $datetext);
-    $smarty->display('menupost.tpl');
 } else {
-    header("Location: login.php");
+    //$datetimeがなかったらカレンダーにリダイレクトする
+    header("Location: calendar.php");
 }
+
+// Smarty接続
+$smarty = getSmarty();
+$smarty->assign('menus', $menus);
+$smarty->assign('date', $date);
+$smarty->assign('datetext', $datetext);
+$smarty->display('menupost.tpl');
