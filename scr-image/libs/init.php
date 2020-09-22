0f8bd3756f9ac12c228add9aba3a6431bf1257a2
ことうはると <haruto.k.0321@gmail.com>
2020-09-23 04:50:02 +0900

    APPLICATION_URLの定義

--- scr-image/libs/init.php
+++ scr-image/libs/init.php
@@ -1,5 +1,7 @@
 <?php
 const APPLICATION_DIR = __DIR__. '/../';
+const APPLICATION_URL_LIBS = __DIR__. '/';
+
 require_once APPLICATION_DIR. 'libs/smarty/Smarty.class.php';
 require_once APPLICATION_DIR. 'vendor/autoload.php';
 require_once APPLICATION_DIR. 'libs/function.php';
